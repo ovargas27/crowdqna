ce67e1a14c272cc05ce5b48587b744108dcdf9a4
David Padilla <david.padilla@crowdint.com>
2010-05-27 20:49:29 -0500

    Heroku friendly settings

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,4 +1,4 @@
 if Settings.table_exists?
-  Settings.admin_user = 'abc'
-  Settings.admin_password = 'defg'
+  Settings.admin_user = (ENV['admin_user'] ? ENV['admin_user'] : 'abc')
+  Settings.admin_password = (ENV['admin_password'] ? ENV['admin_password'] : 'abc')
 end
